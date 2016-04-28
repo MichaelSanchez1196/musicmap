08e70b78d799b15d50b1cf915e0c7a1e0af0d632
Alyssia Goodwin <alyssiagoodwin@gmail.com>
2016-04-28 00:11:56 -0700

    Added files via upload
    This is a basic map of all the states, opens on the internet

--- map.py
+++ map.py
@@ -1,1 +1,14 @@
-print "Hello World"
+import plotly.plotly as py
+import plotly.tools as tls
+import pandas as pd
+import plotly.graph_objs as go
+tls.set_credentials_file(username='algoodwin', api_key='fnqzr6li9g')
+trc = dict(
+    type = 'choropleth',
+    #locations = ['AL', 'AK','AZ','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'],
+    locationmode ='USA-states',
+    colorscale = ['greyscale'],
+    z = [10,20,40])
+lyt = dict(geo=dict(scope='usa'))
+map = go.Figure(data=[trc],layout = lyt)
+py.plot(map)
